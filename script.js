a27b9dc82a0bc0c10ea3be4120eb644172d95f16
Developer0Rohit <rkdjsoundrewasa1234@gmail.com>
2023-10-28 23:47:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var x=  document.getElementById("login");
+var y=document.getElementById("register");
+var z=document.getElementById("btn");
+
+function register(){
+    x.style.left="-500px";
+    y.style.left="10px";
+    z.style.left="110px";
+
+}
+
+function login(){
+    x.style.left="10px";
+    y.style.left="500px";
+    z.style.left="0px";
+
+}
